b995f037eb96490d3c48ca4fbfaf19cec89712a8
hackers-pr <hackers-pr@users.noreply.github.com>
2021-11-24 16:31:30 +0300

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# random_words
+is a very simple program to show random words
+
+# Usage
+```bash
+main.rb [options]
+````
--- main.rb
+++ main.rb
@@ -6,24 +6,48 @@ require 'net/http'
 
 class RandomWords
   def initialize
-    @filename = 'words.txt'
+    @parser = OptionParser.new
+    @file = 'words.txt'
   end
 
   def parse
-    OptionParser.new do |opts|
-      opts.on('-f', '--filename FILENAME') { |n| @filename = n }
-      opts.on('-d', '--download') { download }
-      opts.on('-g', '--get N', Integer) { |n| get(n) }
-    end.parse!
+    define_options
+
+    ARGV << '-h' if ARGV.empty?
+    @parser.parse!
   end
 
-  def download
-    uri = URI('https://petscan.wmflabs.org/?format=plain&psid=20679439')
-    File.write(@filename, Net::HTTP.get(uri))
+  def define_options
+    @parser.banner = "Usage: #{__FILE__} [options]"
+
+    %w[file download get help].each { |option| send("define_#{option}_option") }
   end
 
-  def get(n)
-    puts IO.readlines(@filename).sample(n)
+  def define_file_option
+    @parser.on('-f', '--filename FILENAME',
+               'path to the file with which the program will operate') { |file| @file = file }
+  end
+
+  def define_download_option
+    @parser.on('-d', '--download', 'download words and save them to the file') do
+      uri = URI('https://petscan.wmflabs.org/?format=plain&psid=20679439')
+      File.write(@file, Net::HTTP.get(uri))
+    end
+  end
+
+  def define_get_option
+    @parser.on('-g', '--get N', Integer, 'show N random words from the file') do |n|   
+      raise ArgumentError, 'file does not exist' unless File.file?(@file)
+
+      puts IO.readlines(@file).sample(n)
+    end
+  end
+
+  def define_help_option
+    @parser.on('-h', '--help', 'show this message') do
+      puts @parser
+      exit
+    end
   end
 end
 
